dc6cba43ea2d3183e6c64af565f08dc3efc67a1c
Gianluca Esposito <g@esposi.to>
2020-07-12 15:32:01 +0200

    Upgrade gatsby-starter-default

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,51 @@ module.exports = {
     title: 'DevDay Events',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'DevDay Events',
         short_name: 'DevDay',
-        icon: 'src/images/android-chrome-192x192.png', // This path is relative to the root of the site.
-        theme_color: '#ffffff',
+        start_url: `/`,
         background_color: '#ffffff',
-        display: 'standalone',
+        theme_color: '#ffffff',
+        display: `minimal-ui`,
+        icon: 'src/images/android-chrome-192x192.png', // This path is relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: `gatsby-plugin-webfonts`,
       options: {
-        fonts: [`montserrat`],
+        fonts: {
+          google: [
+            {
+              family: 'Montserrat',
+              // variants: ['300', '400', '500'],
+              // subsets: ['latin']
+              // text: 'Hello'
+              // fontDisplay: 'swap',
+              // strategy: 'selfHosted' // 'base64' || 'cdn'
+            },
+          ],
+        },
+        //formats: ['woff2', 'woff'],
+        //useMinify: true,
+        //usePreload: true,
+        //usePreconnect: false,
       },
     },
-    'gatsby-plugin-offline',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
